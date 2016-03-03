4c523a49b094a7ab57f76ccfda86685aa3f00114
lukaspospisil <lukas.pospisil@vsb.cz>
2016-03-03 12:34:35 +0100

    CUDA check error with ifdef USE_GPU

--- include/common.h
+++ include/common.h
@@ -90,16 +90,18 @@ class Timer {
 };
 
 
-/* cuda error check */ // TODO: if USE_GPU
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess) 
-   {
-      fprintf(stderr,"\n\x1B[31mCUDA error:\x1B[0m %s %s \x1B[33m%d\x1B[0m\n\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
+/* cuda error check */ 
+#ifdef USE_GPU
+	#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+	inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+	{
+		if (code != cudaSuccess) 
+		{
+			fprintf(stderr,"\n\x1B[31mCUDA error:\x1B[0m %s %s \x1B[33m%d\x1B[0m\n\n", cudaGetErrorString(code), file, line);
+			if (abort) exit(code);
+		}
+	}
+#endif
 
 }
 
