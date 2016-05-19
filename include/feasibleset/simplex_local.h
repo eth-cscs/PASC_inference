eca377acd6886cbfba35f98f20388b9a6fb0cb24
lukaspospisil <lukas.pospisil@vsb.cz>
2016-05-19 21:31:59 +0200

    kernels added

--- include/feasibleset/simplex_local.h
+++ include/feasibleset/simplex_local.h
@@ -83,7 +83,17 @@ class SimplexFeasibleSet_Local: public GeneralFeasibleSet<PetscVector> {
 		 */		
 		void project(GeneralVector<PetscVector> &x);
 
+		void project_sub(int t, double *x, int T, int K);
+		void sort_bubble(double *x, int n);
+		
+		
 };
+
+/* cuda kernels cannot be a member of class */
+#ifdef USE_CUDA
+__device__ void device_sort_bubble(double *x, int n);
+__global__ void project_kernel(double *x, int T, int K);
+#endif
 
 } // end of namespace
 
@@ -140,21 +150,17 @@ void SimplexFeasibleSet_Local::project(GeneralVector<PetscVector> &x) {
 	
 	TRY( VecGetArray(x.get_vector(),&x_arr) );
 
+#ifdef USE_GPU
+	/* use kernel to compute projection */
+	//TODO: here should be actually the comparison of Vec type! not simple use_gpu
+	project_kernel<<<T, 1>>>(x_local_arr,T,K_local);
+#else
+	/* use openmp */
 	#pragma omp parallel for
 	for(t=0;t<T;t++){
-		/* cut x_sub from x */
-		for(k=0;k<K_local;k++){
-			x_sub[k] = x_arr[k*T+t];
-		}
-		
-		/* compute subprojection */
-		get_projection_sub(x_sub, this->K_local);
-
-		/* add x_sub back to x */
-		for(k=0;k<K_local;k++){
-			x_arr[k*T+t] = x_sub[k];
-		}
+		project_sub(t,x_local_arr,T,K_local);
 	}
+#endif
 
 	TRY( VecRestoreArray(x.get_vector(),&x_arr) );
 
@@ -162,81 +168,7 @@ void SimplexFeasibleSet_Local::project(GeneralVector<PetscVector> &x) {
 }
 
 
-/* project x_sub to feasible set defined by equality and inequality constraints
- * sum(x_sub) = 1
- * x_sub >= 0
- * 
- * NEVER LOG THIS FUNCTION! (lots of calls)
- */
-void SimplexFeasibleSet_Local::get_projection_sub(double *x_sub, int n){
-	int i;
-
-	bool is_inside = true;
-	double sum = 0.0;
-	
-	/* control inequality constraints */
-	for(i = 0; i < n; i++){ // TODO: could be performed parallely  
-		if(x_sub[i] < 0.0){
-			is_inside = false;
-		}
-		sum += x_sub[i];
-	}
-
-	/* control equality constraints */
-	if(sum != 1){ 
-		is_inside = false;
-	}
-
-
-	/* if given point is not inside the feasible domain, then do projection */
-	if(!is_inside){
-		int j;
-		/* compute sorted x_sub */
-		double y[n], sum_y;
-		for(i=0;i<n;i++){
-			y[i] = x_sub[i]; 
-		}
-		sort_bubble(y,n);
-
-		/* now perform analytical solution of projection problem */	
-		double t_hat = 0.0;
-		i = n - 1;
-		double ti;
-
-		while(i >= 1){
-			/* compute sum(y) */
-			sum_y = 0.0;
-			for(j=i;j<n;j++){ /* sum(y(i,n-1)) */
-				sum_y += y[j];
-			}
-				
-			ti = (sum_y - 1.0)/(double)(n-i);
-			if(ti >= y[i-1]){
-				t_hat = ti;
-				i = -1; /* break */
-			} else {
-				i = i - 1;
-			}
-		}
-
-		if(i == 0){
-			t_hat = (sum-1.0)/(double)n; /* uses sum=sum(x_sub) */
-		}
-    
-		for(i = 0; i < n; i++){ // TODO: could be performed parallely  
-			/* (*x_sub)(i) = max(*x_sub-t_hat,0); */
-			ti = x_sub[i] - t_hat;	
-			if(ti > 0.0){
-				x_sub[i] = ti;
-			} else {
-				x_sub[i] = 0.0;
-			}
-		}
-	}
-}
-
-/* sort values of scalar vector */
-void SimplexFeasibleSet_Local::sort_bubble(double *x, int n){
+void sort_bubble(double *x, int n){
 	int i;
 	int m = n;
 	int mnew;
@@ -257,6 +189,213 @@ void SimplexFeasibleSet_Local::sort_bubble(double *x, int n){
 		m = mnew;
     }
 }
+
+/* this will be a kernel function which computes a projection of a point onto simplex in nD
+ *
+ * just for curiosity more details: 
+ * take K-dimensional vector x[t,t+T,t+2T,...t+(K-1)T] =: p
+ * and compute projection
+ * P(p) = arg min || p - y ||_2
+ * subject to constraints (which define simplex)
+ * y_0 + ... y_{K-1} = 1
+ * y_i >= 0 for all i=0,...,K-1
+ *
+ * in practical applications K is much more lower number than T
+ * K - number of clusters (2 - 10^2)
+ * T - length of time-series (10^5 - 10^9) 
+ */ 
+void project_sub(int t, double *x, int T, int K){
+	if(t<T){ /* maybe we call more than T kernels */
+		int k;
+
+		bool is_inside = true;
+		double sum = 0.0;
+	
+		/* control inequality constraints */
+		for(k = 0; k < K; k++){ // TODO: could be performed parallely  
+			if(x[k*T+t] < 0.0){
+				is_inside = false;
+			}
+			sum += x[k*T+t];
+		}
+
+		/* control equality constraints */
+		if(sum != 1){ 
+			is_inside = false;
+		}
+
+		/* if given point is not inside the feasible domain, then do projection */
+		if(!is_inside){
+			int j,i;
+			/* compute sorted x_sub */
+			double *y = new double[K];
+			double sum_y;
+			for(k=0;k<K;k++){
+				y[k] = x[k*T+t]; 
+			}
+			sort_bubble(y,K);
+
+			/* now perform analytical solution of projection problem */	
+			double t_hat = 0.0;
+			i = K - 1;
+			double ti;
+
+			while(i >= 1){
+				/* compute sum(y) */
+				sum_y = 0.0;
+				for(j=i;j<K;j++){ /* sum(y(i,n-1)) */
+					sum_y += y[j];
+				}
+				
+				ti = (sum_y - 1.0)/(double)(K-i);
+				if(ti >= y[i-1]){
+					t_hat = ti;
+					i = -1; /* break */
+				} else {
+					i = i - 1;
+				}
+			}
+
+			if(i == 0){
+				t_hat = (sum-1.0)/(double)K; /* uses sum=sum(x_sub) */
+			}
+    
+			for(k = 0; k < K; k++){ // TODO: could be performed parallely  
+				/* (*x_sub)(i) = max(*x_sub-t_hat,0); */
+				ti = x[k*T+t] - t_hat;	
+				if(ti > 0.0){
+					x[k*T+t] = ti;
+				} else {
+					x[k*T+t] = 0.0;
+				}
+			}
+			
+			delete y;
+		}
+		
+	}
+
+	/* if t >= T then relax and do nothing */	
+}
+
+
+
+
+/* kernels in cuda */
+#ifdef USE_CUDA
+
+__device__ void device_sort_bubble(double *x, int n){
+	int i;
+	int m=n;
+	int mnew;
+	double swap;
+
+	while(m > 0){
+		/* Iterate through x */
+		mnew = 0;
+		for(i=1;i<m;i++){
+			/* Swap elements in wrong order */
+			if (x[i] < x[i - 1]){
+				swap = x[i];
+				x[i] = x[i-1];
+				x[i-1] = swap;
+				mnew = i;
+			}
+	        }
+		m = mnew;
+	}
+}
+
+/* this is a kernel function which computes a projection of a point onto simplex in nD
+ *
+ * take K-dimensional vector x[t,t+T,t+2T,...t+(K-1)T] =: p
+ * and compute projection
+ * P(p) = arg min || p - y ||_2
+ * subject to constraints (which define simplex)
+ * y_0 + ... y_{K-1} = 1
+ * y_i >= 0 for all i=0,...,K-1
+ *
+ * in practical applications K is much more lower number than T
+ * K - number of clusters (2 - 10^2)
+ * T - length of time-series (10^5 - 10^9) 
+ */ 
+__global__ void project_kernel(double *x, int T, int K){
+	int t = blockIdx.x*blockDim.x + threadIdx.x; /* compute my id */
+
+	if(t<T){ /* maybe we call more than T kernels */
+		int k;
+
+		bool is_inside = true;
+		double sum = 0.0;
+	
+		/* control inequality constraints */
+		for(k = 0; k < K; k++){ // TODO: could be performed parallely  
+			if(x[k*T+t] < 0.0){
+				is_inside = false;
+			}
+			sum += x[k*T+t];
+		}
+
+		/* control equality constraints */
+		if(sum != 1){ 
+			is_inside = false;
+		}
+
+		/* if given point is not inside the feasible domain, then do projection */
+		if(!is_inside){
+			int j,i;
+			/* compute sorted x_sub */
+			double *y = new double[K];
+			double sum_y;
+			for(k=0;k<K;k++){
+				y[k] = x[k*T+t]; 
+			}
+			device_sort_bubble(y,K);
+
+			/* now perform analytical solution of projection problem */	
+			double t_hat = 0.0;
+			i = K - 1;
+			double ti;
+
+			while(i >= 1){
+				/* compute sum(y) */
+				sum_y = 0.0;
+				for(j=i;j<K;j++){ /* sum(y(i,n-1)) */
+					sum_y += y[j];
+				}
+				
+				ti = (sum_y - 1.0)/(double)(K-i);
+				if(ti >= y[i-1]){
+					t_hat = ti;
+					i = -1; /* break */
+				} else {
+					i = i - 1;
+				}
+			}
+
+			if(i == 0){
+				t_hat = (sum-1.0)/(double)K; /* uses sum=sum(x_sub) */
+			}
+    
+			for(k = 0; k < K; k++){ // TODO: could be performed parallely  
+				/* (*x_sub)(i) = max(*x_sub-t_hat,0); */
+				ti = x[k*T+t] - t_hat;	
+				if(ti > 0.0){
+					x[k*T+t] = ti;
+				} else {
+					x[k*T+t] = 0.0;
+				}
+			}
+			
+			delete y;
+		}
+		
+	}
+
+	/* if t >= T then relax and do nothing */	
+}
+
+#endif
 
 
 
