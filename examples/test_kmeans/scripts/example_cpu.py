cd4450dc81b57e591d41578285da4e03e22a3e4a
lukaspospisil <lukas.pospisil@vsb.cz>
2016-06-23 12:18:19 +0200

    results computed on PIZ DAINT

--- examples/test_kmeans/scripts/example_cpu.py
+++ examples/test_kmeans/scripts/example_cpu.py
@@ -8,18 +8,18 @@ from myscripts import commit_batchfiles
 from myscripts import show_jobs
 
 # times to compute
-#Ts = [1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000];
-Ts = [1000, 5000];
+Ts = [1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000];
+#Ts = [1000, 5000];
 
 # on how many nodes to compute
-#Ns = [1, 2, 3, 4, 5, 6];
-Ns = [1, 2];
+Ns = [1, 2, 3, 4, 5, 6];
+#Ns = [1, 2];
 
 # name of problem
 problem_name = "test_kmeans"
-problem_parameters = "--test_K=3 --spgqpsolver_eps=0.00001 --spgqpsolver_stop_Anormgp=false --spgqpsolver_stop_normgp_normb=true --spgqpsolver_debug_mode=3 --tssolver_debug_mode=3 --spgqpsolver_dotfloor=2 --spgqpsolver_m=20 --test_epssqr=1 --test_shortinfo=true --test_savevtk=false --test_savecsv=false";
+problem_parameters = "--test_K=3 --spgqpsolver_eps=0.00001 --spgqpsolver_stop_Anormgp=false --spgqpsolver_stop_normgp_normb=true --spgqpsolver_debug_mode=3 --tssolver_debug_mode=3 --spgqpsolver_dotfloor=3 --spgqpsolver_m=20 --test_epssqr=10 --test_shortinfo=true --test_savevtk=false --test_savecsv=false";
 library_path = "~/soft/PASC_inference"
-problem_time = "00:10:00"
+problem_time = "00:15:00"
 
 # generate bash scripts
 write_batchfiles(Ts,Ns,problem_name, problem_time, problem_parameters, library_path, "CPU1", 0, 1)
