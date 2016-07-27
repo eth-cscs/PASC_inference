9187d083096f45a0744ecf47d2aa007c06b5fcf4
lukaspospisil <lukas.pospisil@vsb.cz>
2016-07-27 11:58:59 +0200

    now compileable on pizdaint with gpu

--- util/cmake/load_cuda.cmake
+++ util/cmake/load_cuda.cmake
@@ -6,13 +6,13 @@ if(${USE_CUDA})
 	set(CUDA_PROPAGATE_HOST_FLAGS off) # if flags are passed with -Xcompiler, they also affect NVCC which doesn't understand all g++ flags we use
 	set(CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER}) # without this, cc is used instead of CC and all include paths have to be specified manually
 	string(TOUPPER "${CMAKE_BUILD_TYPE}" BUILD_TYPE_UPPER)
-	set(CUDA_CXX_FLAGS "${CMAKE_CXX_FLAGS}  -lcusparse -Wno-vla ${CMAKE_CXX_FLAGS_${BUILD_TYPE_UPPER}}") # add flags specific to build type
+	set(CUDA_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lssl -lcusparse -Wno-vla ${CMAKE_CXX_FLAGS_${BUILD_TYPE_UPPER}}") # add flags specific to build type
 	string(REPLACE "-std=c++11" "" CUDA_CXX_FLAGS ${CUDA_CXX_FLAGS}) # remove C++11 from options
 	string(REPLACE "-Wall" "" CUDA_CXX_FLAGS ${CUDA_CXX_FLAGS}) # remove Wall from options
 
 	set(FLAGS_DEF "-USE_CUDA ${FLAGS_DEF}")
 	set(FLAGS_DEF_D "-DUSE_CUDA ${FLAGS_DEF_D}")
-	set(LIBRARIES_DEF "${LIBRARIES_DEF}")
+	set(LIBRARIES_DEF "ssl;cusparse;${LIBRARIES_DEF}")
 endif()
 
 
