89ed7e3859ba0d26dccd6cbee2dacaa931c2d3ae
lukaspospisil <lukas.pospisil@vsb.cz>
2016-08-11 12:55:43 +0200

    ready to test script on piz daint

--- examples/test_image/scripts/example_cpu.py
+++ examples/test_image/scripts/example_cpu.py
@@ -7,27 +7,46 @@ from myscripts import write_batchfiles
 from myscripts import commit_batchfiles
 from myscripts import show_jobs
 
-# times to compute
-Ts = [1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000];
-#Ts = [100000];
+# path to PASC_inference library
+library_path = "~/soft/PASC_inference";
+
+# image_path = [image_dir]/[begin]_[width]_[height]_[noise].bin
+image_name = "usi";
+image_dir = "data/usi_text";
+
+# dimensions of images to compute [width,height]
+dimensions = [[250,150]];
+#dimensions = [[250,150],[500,300],[1000,600]];
+
+# noise of input images (part of the name of image)
+noises = ['00'];
+#noises = ['00','02','04','10'];
+
+# used penalty
+epssqrs = [1e-5];
+#epssqrs = [1e-5, 1e-6, 1e-7];
+
+# how many clusters to use (value of K)
+Ks = [2];
 
 # on how many nodes to compute
-Ns = [1, 2, 3, 4, 5, 6];
-#Ns = [1, 2];
+Ns = [1];
 
-# name of problem
-problem_name = "test_kmeans"
-problem_parameters = "--test_K=3 --spgqpsolver_eps=0.0001 --spgqpsolver_stop_Anormgp_normb=true --spgqpsolver_stop_normgp_normb=true --spgqpsolver_debug_mode=3 --spgqpsolver_maxit=500 --tssolver_debug_mode=3 --spgqpsolver_dotfloor=2 --spgqpsolver_m=20 --test_epssqr=10 --test_savecsv=false --test_savevtk=false"
-library_path = "~/soft/PASC_inference"
-problem_time = "00:10:00"
+# name of problem (name of compiled program)
+problem_name = "test_image";
+
+# common parameters
+problem_parameters = "--test_cutdata=true --test_scaledata=false --test_annealing=10 --tssolver_debug_mode=2 --spgqpsolver_maxit=2000 --tssolver_maxit=100 --spgqpsolver_debug_mode=0";
+
+# the upper estimation of computing time
+problem_time = "00:10:00"; 
 
 # generate bash scripts
-write_batchfiles(Ts,Ns,problem_name, problem_time, problem_parameters, library_path, "CPU1", 0, 1)
-write_batchfiles(Ts,Ns,problem_name, problem_time, problem_parameters, library_path, "CPU8", 0, 8)
+batchfile_list = write_batchfiles(image_dir, image_name, dimensions, noises, epssqrs, Ks, Ns, problem_name, problem_time, problem_parameters, library_path, "CPU8", 8, 0);
 
 # run bash scripts
-commit_batchfiles(Ts,Ns,problem_name, "CPU1", "c11", "normal")
-commit_batchfiles(Ts,Ns,problem_name, "CPU8", "c11", "normal")
+commit_batchfiles(batchfile_list, "c11", "normal")
 
 # show the queue
-show_jobs("c11")
+#show_jobs("c11")
+
--- examples/test_image/scripts/myscripts.py
+++ examples/test_image/scripts/myscripts.py
@@ -15,52 +15,56 @@ if not os.path.exists("shortinfo"):
     os.makedirs("shortinfo")
 
 # define function for writing a lot of batchscripts
-def write_batchfiles(Ts,Ns,problem_name, problem_time, problem_parameters, library_path, architecture, n_gpu, n_threads):
+def write_batchfiles(image_dir, image_name, dimensions, noises, epssqrs, Ks, Ns, problem_name, problem_time, problem_parameters, library_path, architecture, Nthreads, Ngpu):
     "this function creates a lot of bash scripts"
     # say hello
-    print "Preparing batch scripts: %s (%d threads, %d gpus)" % (architecture,n_threads,n_gpu)
-    for t in Ts:
-        for n in Ns:
-            print " - N = %d, T = %d" % (n,t);
-            write_batchfile(problem_name, problem_time, problem_parameters, library_path, architecture, n_gpu, n_threads, n, t)
-    return
+    print "Preparing batch scripts: %s (Nthreads=%d, Ngpu=%d)" % (architecture,Nthreads,Ngpu)
+    batchfile_list = [];
+    for dimension in dimensions:
+        for noise in noises:
+            for epssqr in epssqrs:
+                for K in Ks:
+                    for N in Ns:
+                        image_path = "%s/%s_%s_%s_%s.bin" % (image_dir,image_name,dimension[0],dimension[1],noise);
+                        problem_name_full = "%s_%s_w%s_h%s_noise%s_epssqr%f_K%s_arch%s_N%s_Nthreads%s_Ngpu%s" % (problem_name,image_name,dimension[0],dimension[1],noise,epssqr,K,architecture,N,Nthreads,Ngpu)
+                        print " - %s: %s" % (problem_name, problem_name_full);
+                        problem_parameters_full = "%s --test_image_filename=\"%s\" --test_image_out=\"%s\" --test_width=%s --test_height=%s --test_epssqr=%f --test_K=%s " % (problem_parameters, image_path, problem_name_full, dimension[0], dimension[1], epssqr, K);
+                        batchfile_name = write_batchfile(problem_name, problem_name_full, problem_time, problem_parameters_full, library_path, architecture, N, Nthreads, Ngpu);
+                        batchfile_list.append(batchfile_name);
+    return batchfile_list
 
 # define function for writing a fun into file
-def write_batchfile(problem_name, problem_time, problem_parameters, library_path, architecture, n_gpu, n_threads, n, t):
+def write_batchfile(problem_name, problem_name_full, problem_time, problem_parameters, library_path, architecture, N, Nthreads, Ngpu):
     "this function prints a fun into batch script file, the fun is based on parameters"
-    myfile = open("./batch/%s_%s_N%dT%d.batch" % (problem_name,architecture,n,t), 'w+');
+    batchfile_name = "./batch/%s.batch" % (problem_name_full);
+    myfile = open(batchfile_name, 'w+');
     # write some funny stuff into file
     myfile.write("#!/bin/bash -l\n")
     myfile.write("\n## sbatch settings\n")
-    myfile.write("#SBATCH --nodes=%d\n" % (n))
+    myfile.write("#SBATCH --nodes=%d\n" % (N))
     myfile.write("#SBATCH --ntasks-per-core=1\n")
-    myfile.write("#SBATCH --ntasks=%d\n" %(n))
-    myfile.write("#SBATCH --gres=gpu:%d\n" % (n_gpu))
+    myfile.write("#SBATCH --ntasks=%d\n" %(N))
+    myfile.write("#SBATCH --gres=gpu:%d\n" % (Ngpu))
     myfile.write("#SBATCH --time=%s\n" % (problem_time))
     myfile.write("#SBATCH --partition=normal\n")
-    myfile.write("#SBATCH --output=%s.%%j.o\n" % (problem_name))
-    myfile.write("#SBATCH --error=%s.%%j.e\n" % (problem_name))
+    myfile.write("#SBATCH --output=%s.%%j.o\n" % (problem_name_full))
+    myfile.write("#SBATCH --error=%s.%%j.e\n" % (problem_name_full))
     myfile.write("\n## load modules\n")
     myfile.write("source %s/util/module_load_daint\n" % (library_path))
     myfile.write("source %s/util/set_petsc_daint\n" % (library_path))
     myfile.write("\n## set number of threads\n")
-    myfile.write("export OMP_NUM_THREADS=%d\n" % (n_threads))
+    myfile.write("export OMP_NUM_THREADS=%d\n" % (Nthreads))
     myfile.write("\n## run the job\n")
-    myfile.write("srun -N %d -n %d -T 1 ./test_kmeans_load" % (n,n))
-    myfile.write(" --test_data_filename='data/data_kmeans_T%d.bin' --test_gamma0_filename='data/gamma0_kmeans_T%dK3.bin'" % (t,t))
-    myfile.write(" --test_shortinfo_header='architecture,nodes,threads,T,K,' --test_shortinfo_values='%s,%d,%d,%d,3,' --test_shortinfo_filename='shortinfo/kmeans_%s_N%d_threads%d_T%d_K3.txt'" % (architecture,n,n_threads,t,architecture,n,n_threads,t))
-    myfile.write(" %s\n" % (problem_parameters))
-    return
+    myfile.write("srun -N %d -n %d -T 1 ./%s %s\n" % (N,N,problem_name,problem_parameters))
+    return batchfile_name
 
-def commit_batchfiles(Ts,Ns,problem_name, architecture, account, partition):
+def commit_batchfiles(batchfile_list, account, partition):
     "this function commits batch files"
     # say hello
-    print "Commiting batch scripts: %s" % (architecture)
-    for t in Ts:
-        for n in Ns:
-            filename = "batch/%s_%s_N%dT%d.batch" % (problem_name,architecture,n,t)
-            print  " - %s" % (filename);
-            call(["sbatch", filename, "--account=%s" % (account), "--partition=%s" % (partition)])
+    print "Commiting batch scripts: "
+    for batchfile_name in batchfile_list:
+        print  " - %s" % (batchfile_name);
+#        call(["sbatch", batchfile_name, "--account=%s" % (account), "--partition=%s" % (partition)])
     return
 
 def show_jobs(account):
