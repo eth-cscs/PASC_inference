24708fe72e83148974bc919e609a28dca7159f6d
lukaspospisil <lukas.pospisil@vsb.cz>
2016-08-11 14:38:37 +0200

    given theta added

--- examples/test_image/scripts/example_cpu.py
+++ examples/test_image/scripts/example_cpu.py
@@ -15,16 +15,16 @@ image_name = "usi";
 image_dir = "data/usi_text";
 
 # dimensions of images to compute [width,height]
-dimensions = [[250,150]];
-#dimensions = [[250,150],[500,300],[1000,600]];
+#dimensions = [[250,150]];
+dimensions = [[250,150],[500,300],[1000,600]];
 
 # noise of input images (part of the name of image)
-noises = ['00'];
-#noises = ['00','02','04','10'];
+#noises = ['00'];
+noises = ['00','02','04','10'];
 
 # used penalty
-epssqrs = [1e-5];
-#epssqrs = [1e-5, 1e-6, 1e-7];
+#epssqrs = [1e-5];
+epssqrs = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2];
 
 # how many clusters to use (value of K)
 Ks = [2];
@@ -36,7 +36,7 @@ Ns = [1];
 problem_name = "test_image";
 
 # common parameters
-problem_parameters = "--test_cutdata=true --test_scaledata=false --test_annealing=10 --tssolver_debug_mode=2 --spgqpsolver_maxit=2000 --tssolver_maxit=100 --spgqpsolver_debug_mode=0 --test_shortinfo=true ";
+problem_parameters = "--test_cutdata=true --test_scaledata=false --test_annealing=10 --tssolver_debug_mode=2 --spgqpsolver_maxit=2000 --tssolver_maxit=100 --spgqpsolver_debug_mode=0 --test_shortinfo=true --test_Theta=0.4 --test_Theta=0.5";
 
 # the upper estimation of computing time
 problem_time = "00:10:00"; 
