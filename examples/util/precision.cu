fb241d0a2b34fa299e951b3bcbb13734367a7156
lukaspospisil <lukas.pospisil@vsb.cz>
2016-11-30 13:18:18 +0100

    testing script for printing precision added

---
+++ examples/util/precision.cu
@@ -0,0 +1,54 @@
+/** @file precision.cu
+ *  @brief test the precision of stored values (float/double?)
+ *
+ *  @author Lukas Pospisil
+ */
+
+#include "pascinference.h"
+#include <math.h>
+
+using namespace pascinference;
+
+#ifdef USE_CUDA
+__global__ void print_precision(){
+	double a;
+	for(int i=0; i < 25;i++){
+		a = 1.0 + pow(10.0,-i);
+		printf(" 1+10^{- %d} : %.*e\n", i, 30, a );
+	}
+}
+
+#endif
+
+
+int main( int argc, char *argv[] )
+{
+	/* call initialize */
+	if(!Initialize(argc, argv)){
+		return 0;
+	} 
+	
+	coutMaster << std::setprecision(30);
+	coutMaster << "CPU:" << std::endl;
+
+	double a;
+	for(int i=0; i < 25;i++){
+		a = 1.0 + pow(10.0,-i);
+		coutMaster << " 1+10^{-" << std::setw(3) << i << "}: " << a << std::endl;
+	}
+
+
+#ifdef USE_CUDA
+	coutMaster << std::endl;
+	coutMaster << "GPU:" << std::endl;
+
+	print_precision<<<1, 1>>>();
+	gpuErrchk( cudaDeviceSynchronize() );
+
+#endif
+
+	Finalize();
+	
+	return 0;
+}
+
--- examples/util/util.cmake
+++ examples/util/util.cmake
@@ -4,6 +4,7 @@ include_directories("${CMAKE_SOURCE_DIR}/util/")
 option(UTIL "UTIL" OFF)
 option(UTIL_DIFF_NORM_VEC "UTIL_DIFF_NORM_VEC" OFF)
 option(UTIL_STAT_VEC "UTIL_STAT_VEC" OFF)
+option(UTIL_PRECISION "UTIL_PRECISION" OFF)
 
 if(${UTIL})
 	# define shortcut to compile all utils
@@ -19,6 +20,7 @@ print("\nUtil")
 printinfo_onoff(" UTIL                                      (...)                      " "${UTIL}")
 printinfo_onoff("  UTIL_DIFF_NORM_VEC                        (ConsoleArg)               " "${UTIL_DIFF_NORM_VEC}")
 printinfo_onoff("  UTIL_STAT_VEC                             (ConsoleArg)               " "${UTIL_STAT_VEC}")
+printinfo_onoff("  UTIL_PRECISION                            (ConsoleArg)               " "${UTIL_PRECISION}")
 
 if(${UTIL_DIFF_NORM_VEC})
 	pascadd_executable("util/diff_norm_vec.cpp" "diff_norm_vec")
@@ -28,4 +30,12 @@ if(${UTIL_STAT_VEC})
 	pascadd_executable("util/stat_vec.cpp" "stat_vec")
 endif()
 
+if(${UTIL_PRECISION})
+	# this is signal processing test
+	if(${USE_CUDA})
+		pascadd_executable("util/precision.cu" "precision")
+	else()
+		pascadd_executable("util/precision.cpp" "precision")
+	endif()
+endif()
 
