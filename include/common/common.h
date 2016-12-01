7a638f51a4720d485f6c209a0e53fce999a30c20
lukaspospisil <lukas.pospisil@vsb.cz>
2016-12-01 14:19:15 +0100

    cuda warm up added

--- include/common/common.h
+++ include/common/common.h
@@ -58,16 +58,20 @@ bool Initialize(int, char**);
 void Finalize();
 
 #ifdef USE_CUDA
-	/* cuda error check */ 
-	#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-	inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+/* cuda error check */ 
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+	if (code != cudaSuccess) 
 	{
-		if (code != cudaSuccess) 
-		{
-			fprintf(stderr,"\n\x1B[31mCUDA error:\x1B[0m %s %s \x1B[33m%d\x1B[0m\n\n", cudaGetErrorString(code), file, line);
-			if (abort) exit(code);
-		}
+		fprintf(stderr,"\n\x1B[31mCUDA error:\x1B[0m %s %s \x1B[33m%d\x1B[0m\n\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
 	}
+}
+	
+__global__ void kernel_warmup(){
+	
+}
 #endif
 
 
@@ -103,6 +107,12 @@ bool Initialize(int argc, char *argv[]){
 		#endif
 
 		petscvector::PETSC_INITIALIZED = true;
+	#endif
+	
+	/* cuda warm up */
+	#ifdef USE_CUDA
+		kernel_warmup<<<1,1>>();
+		gpuErrchk( cudaDeviceSynchronize() );
 	#endif
 	
 	return true;
