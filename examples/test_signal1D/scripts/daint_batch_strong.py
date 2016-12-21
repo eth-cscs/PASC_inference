70d9f1ff2f4910a58e7a299f5a18f149ed9f768a
lukaspospisil <lukas.pospisil@vsb.cz>
2016-12-21 22:51:22 +0100

    working on python batch scripts on daint

--- examples/test_signal1D/scripts/daint_batch_strong.py
+++ examples/test_signal1D/scripts/daint_batch_strong.py
@@ -9,12 +9,12 @@ from common_batch import show_jobs
 import os, shutil, sys, getopt
 
 # parse input arguments
-if len(sys.argv) < 3:
-    print 'daint_batch_strong.py <inputfile> <spgqpsolver_eps>'
+if len(sys.argv) < 2:
+    print 'daint_batch_strong.py <inputfile>'
     sys.exit()
 
 inputfile = sys.argv[1];
-spgqpsolver_eps = sys.argv[2];
+spgqpsolver_eps = "1e-4";
 
 # path to exec folder
 username = "pospisil"
@@ -32,7 +32,7 @@ params_list.append("--test_filename_gamma0=data/%s_gamma0.bin" %(inputfile))
 params_list.append("--test_cutdata=false --test_scaledata=false")
 params_list.append("--test_epssqr=1e-2 --test_annealing=1")
 params_list.append("--tssolver_maxit=1 --tssolver_debugmode=0")
-params_list.append("--spgqpsolver_maxit=10000 --spgqpsolver_debugmode=0 --spgqpsolver_stop_difff=false --spgqpsolver_stop_Anormgp=true")
+params_list.append("--spgqpsolver_maxit=10000 --spgqpsolver_debugmode=0 --spgqpsolver_stop_difff=false --spgqpsolver_stop_normgp=true")
 params_list.append("--test_shortinfo=true")
 params_list.append("--test_K=2 --test_Theta=1.0 --test_Theta=2.0")
 params = ' '.join(params_list);
@@ -51,12 +51,11 @@ batchfile_list = [];
 for index in range(len(N)):
     print "GPU: Preparing batch scripts: %s/%s" % (index+1,len(N))
     problem_name = "%s%d" %(gpu_problem_name,N[index])
-    host_string = gpu_host_string[index]
     exec_path = gpu_exec_path
     params2 = "--test_filename_out=%s --test_shortinfo_header=ngpus, --test_shortinfo_values=%d, --test_shortinfo_filename=shortinfo/%s.txt --spgqpsolver_eps=%s" % (problem_name, N[index], problem_name, spgqpsolver_eps)
     exec_name_full = "%s -n %d %s %s %s > batch_out/%s.log" %(mpiexec, N[index], exec_name, params, params2, problem_name)
-#    batch_filename = os.path.join(gpu_batch_path, "%s.pbs" % (problem_name))
-#    write_batch(problem_name, host_string, batch_filename, exec_path, exec_name_full, gpu_modules_path)
+    batch_filename = os.path.join(gpu_batch_path, "%s.pbs" % (problem_name))
+    write_batch(problem_name, host_string, batch_filename, exec_path, exec_name_full, gpu_modules_path)
 #    batchfile_list.append(batch_filename);
 
 
