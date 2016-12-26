f38c2a8f2a8b4caa6eb7721f0c14fcf63c2abe12
lukaspospisil <lukas.pospisil@vsb.cz>
2016-12-26 21:57:40 +0100

    python script for CPU added

--- examples/test_signal1D/scripts/daint_batch_strong.py
+++ examples/test_signal1D/scripts/daint_batch_strong.py
@@ -38,6 +38,7 @@ params_list.append("--test_shortinfo=true")
 params_list.append("--test_K=2 --test_Theta=1.0 --test_Theta=2.0")
 params = ' '.join(params_list);
 
+
 # GPU
 gpu_problem_name = "strong_G";
 gpu_exec_path = "%s/build_gpu/" %(build_path);
@@ -53,6 +54,24 @@ for index in range(len(N)):
     exec_name_full = "%s -n %d %s %s %s > batch_out/%s.log" %(mpiexec, N[index], exec_name, params, params2, problem_name)
     batch_filename = os.path.join(gpu_batch_path, "%s.batch" % (problem_name))
     write_batch(problem_name, N[index], 1, 1, N[index], 1, problem_time, library_path, gpu_batch_path, exec_name_full)
+    batchfile_list.append(batch_filename);
+
+
+# CPU
+cpu_problem_name = "strong_C";
+cpu_exec_path = "%s/build_cpu/" %(build_path);
+cpu_batch_path = "%s/batch/" %(cpu_exec_path);
+
+# CPU: generate bash scripts
+batchfile_list = [];
+for index in range(len(N)):
+    print "CPU: Preparing batch scripts: %s/%s" % (index+1,len(N))
+    problem_name = "%s%d" %(cpu_problem_name,N[index])
+    exec_path = cpu_exec_path
+    params2 = "--test_filename_out=%s --test_shortinfo_header=ncpus, --test_shortinfo_values=%d, --test_shortinfo_filename=shortinfo/%s.txt --spgqpsolver_eps=%s" % (problem_name, N[index], problem_name, spgqpsolver_eps)
+    exec_name_full = "%s -n %d %s %s %s > batch_out/%s.log" %(mpiexec, N[index], exec_name, params, params2, problem_name)
+    batch_filename = os.path.join(cpu_batch_path, "%s.batch" % (problem_name))
+    write_batch(problem_name, N[index], 1, 1, N[index], 1, problem_time, library_path, cpu_batch_path, exec_name_full)
     batchfile_list.append(batch_filename);
 
 
