b0d1f90defb430b333370d57d64b9657acde86ca
lukaspospisil <lukas.pospisil@vsb.cz>
2017-03-09 13:26:44 +0100

    working on dot example

--- examples/test_classes/algebra/test_dot.cu
+++ examples/test_classes/algebra/test_dot.cu
@@ -56,23 +56,48 @@ int main( int argc, char *argv[] )
 
 	TRYCXX( VecCreate(PETSC_COMM_WORLD,&myvector1_Vec) );
 	TRYCXX( VecCreate(PETSC_COMM_WORLD,&myvector2_Vec) );
-		
-	GeneralVector<PetscVector> myvector1(myvector1_Vec);
-	myvector1.load_global(filename1);
-	GeneralVector<PetscVector> myvector2(myvector2_Vec);
-	myvector2.load_global(filename2);
+
+#ifdef USE_CUDA
+	TRYCXX( VecSetType(myvector1_Vec, VECMPICUDA) );
+	TRYCXX( VecSetType(myvector2_Vec, VECMPICUDA) );
+
+#else
+	TRYCXX( VecSetType(myvector1_Vec, VECMPI) );
+	TRYCXX( VecSetType(myvector2_Vec, VECMPI) );
+#endif
+
+	/* prepare viewer to load from file */
+	PetscViewer mviewer1;
+	PetscViewer mviewer2;
+	TRYCXX( PetscViewerCreate(PETSC_COMM_WORLD, &mviewer1) );
+	TRYCXX( PetscViewerCreate(PETSC_COMM_WORLD, &mviewer2) );
+	TRYCXX( PetscViewerBinaryOpen(PETSC_COMM_WORLD ,filename1.c_str(), FILE_MODE_READ, &mviewer1) );
+	TRYCXX( PetscViewerBinaryOpen(PETSC_COMM_WORLD ,filename2.c_str(), FILE_MODE_READ, &mviewer2) );
+	
+	/* load vector from viewer */
+	TRYCXX( VecLoad(myvector1_Vec, mviewer1) );
+	TRYCXX( VecLoad(myvector2_Vec, mviewer2) );
+
+	/* destroy the viewer */
+	TRYCXX( PetscViewerDestroy(&mviewer1) );
+	TRYCXX( PetscViewerDestroy(&mviewer2) );
 
 	mytimer.stop();
 	coutMaster << "- time load      : " << mytimer.get_value_last() << " s" << std::endl;
 
-	coutMaster << "- dimension of v1: " << myvector1.size() << std::endl;
-	coutMaster << "- dimension of v2: " << myvector2.size() << std::endl;
+	int size1, size2;
+	TRYCXX( VecGetSize(myvector1_Vec, &size1) );
+	TRYCXX( VecGetSize(myvector2_Vec, &size2) );
 
+	coutMaster << "- dimension of v1: " << size1 << std::endl;
+	coutMaster << "- dimension of v2: " << size2 << std::endl;
 	
 	/* we will measure the time of operation */
 	double dot_result;
 	for(int i=0;i<n;i++){
-		dot_result = dot(myvector1,myvector2);
+
+		TRYCXX( VecDot(myvector1_Vec, myvector2_Vec, &dot_result) );
+
 		#ifdef USE_CUDA
 			//gpuErrchk( cudaDeviceSynchronize() );
 		#endif
