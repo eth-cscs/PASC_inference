2a16300e6d31a125e62a913681a1923d0663192e
lukaspospisil <lukas.pospisil@vsb.cz>
2017-06-01 13:44:25 +0200

    woking on cuda

--- util/cmake/load_cuda.cmake
+++ util/cmake/load_cuda.cmake
@@ -6,7 +6,7 @@ if(${USE_CUDA})
 	set(CUDA_PROPAGATE_HOST_FLAGS off) # if flags are passed with -Xcompiler, they also affect NVCC which doesn't understand all g++ flags we use
 	set(CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER}) # without this, cc is used instead of CC and all include paths have to be specified manually
 	string(TOUPPER "${CMAKE_BUILD_TYPE}" BUILD_TYPE_UPPER)
-	set(CUDA_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -lssl -lcusparse -Wno-vla ${CMAKE_CXX_FLAGS_${BUILD_TYPE_UPPER}}") # add flags specific to build type
+	set(CUDA_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lssl -lcusparse -Wno-vla ${CMAKE_CXX_FLAGS_${BUILD_TYPE_UPPER}}") # add flags specific to build type
 	string(REPLACE "-std=c++11" "" CUDA_CXX_FLAGS ${CUDA_CXX_FLAGS}) # remove C++11 from options
 	string(REPLACE "-std=c++0x" "" CUDA_CXX_FLAGS ${CUDA_CXX_FLAGS}) # remove C++0x from options
 	string(REPLACE "-Wall" "" CUDA_CXX_FLAGS ${CUDA_CXX_FLAGS}) # remove Wall from options
