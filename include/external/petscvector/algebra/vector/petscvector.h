ac2fd5c43ee411543dd8c3f773ccd012de5877cb
lukaspospisil <lukas.pospisil@vsb.cz>
2017-06-01 15:53:55 +0200

    working on cuda

--- include/external/petscvector/algebra/vector/petscvector.h
+++ include/external/petscvector/algebra/vector/petscvector.h
@@ -21,7 +21,7 @@
 //	#include <../src/vec/vec/impls/seq/seqcuda/cudavecimpl.h>		/* VecCUDACopyToGPU */
 
 	extern "C" {
-		PetscErrorCode VecCUDACopyToGPU(Vec);
+		extern PetscErrorCode VecCUDACopyToGPU(Vec);
 	}
 
 	/* anselm hotfix */
