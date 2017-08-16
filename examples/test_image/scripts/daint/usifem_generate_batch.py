cd5793a987ac35026d0f3b5bcb5d009fc3795c68
lukaspospisil <lukas.pospisil@vsb.cz>
2017-08-16 17:17:24 +0200

    new reductions

--- examples/test_image/scripts/daint/usifem_generate_batch.py
+++ examples/test_image/scripts/daint/usifem_generate_batch.py
@@ -35,7 +35,7 @@ problem_time = "00:20:00";
 # noise of input signal
 nmbfilesmax = 5;
 sigma = 9;
-fem_reduces= [1.0, 0.5, 0.25, 0.125, 0.0625, 0.0312];
+fem_reduces= [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1];
 
 # used penalty
 epssqrs = [1e-7, 5e-7, 1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1, 1e0, 5e0, 1e1, 5e1, 1e2, 5e2, 1e3];
