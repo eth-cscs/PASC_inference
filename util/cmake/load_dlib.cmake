4028dc479b30a727eab107884f2bd2a3a7025799
lukaspospisil <lukas.pospisil@vsb.cz>
2017-09-22 14:47:09 +0200

    change

--- util/cmake/load_dlib.cmake
+++ util/cmake/load_dlib.cmake
@@ -41,7 +41,7 @@ if(${USE_DLIB})
 #				GIT_SUBMODULES util/dlib
 #				URL ${PASCINFERENCE_ROOT}/util/dlib/
 				PREFIX ${CMAKE_BINARY_DIR}/dlib_build
-				CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DDLIB_ENABLE_ASSERTS=ON -DDLIB_NO_GUI_SUPPORT=OFF -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC"
+				CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DDLIB_ENABLE_ASSERTS=ON -DDLIB_NO_GUI_SUPPORT=OFF "-DCMAKE_CXX_FLAGS=-fPIC -std=c++11" -DCMAKE_C_FLAGS="-fPIC"
 #				EXCLUDE_FROM_ALL TRUE
 #				STEP_TARGETS build
 				INSTALL_COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/dlib_build/src/project_dlib-build/dlib/libdlib.so ${CMAKE_BINARY_DIR}/lib/libdlib.so && ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/dlib_build/src/project_dlib-build/dlib/libdlib.so.19.4.99 ${CMAKE_BINARY_DIR}/lib/libdlib.so.19.4.99
